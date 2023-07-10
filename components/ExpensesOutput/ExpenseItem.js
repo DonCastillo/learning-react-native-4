cb67a7f078cac11e4496627c6874864a1ad89f6c
Don <don.qcastillo@yahoo.com>
2023-07-10 00:57:34 -0600

    added header button, made item card pressable

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -2,13 +2,24 @@ import { View, Text, Pressable, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
-function ExpenseItem({description, amount, date}) {
+function ExpenseItem({ description, amount, date }) {
+    function expensePressHandler() {
+        console.log("pressing an item");
+    }
+
     return (
-        <Pressable>
+        <Pressable
+            onPress={expensePressHandler}
+            style={({ pressed }) => pressed && styles.pressed}
+        >
             <View style={styles.expenseItem}>
                 <View>
-                    <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                    <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+                    <Text style={[styles.textBase, styles.description]}>
+                        {description}
+                    </Text>
+                    <Text style={styles.textBase}>
+                        {getFormattedDate(date)}
+                    </Text>
                 </View>
                 <View style={styles.amountContainer}>
                     <Text style={styles.amount}>{amount.toFixed(2)}</Text>
@@ -21,21 +32,24 @@ function ExpenseItem({description, amount, date}) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+    pressed: {
+        opacity: 0.75,
+    },
     expenseItem: {
         padding: 12,
         marginVertical: 8,
         backgroundColor: GlobalStyles.colors.primary500,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
         borderRadius: 6,
         elevation: 3,
         shadowColor: GlobalStyles.colors.gray500,
         shadowRadius: 4,
         shadowOffset: {
             width: 1,
-            height: 1
+            height: 1,
         },
-        shadowOpacity: .4
+        shadowOpacity: 0.4,
     },
     textBase: {
         color: GlobalStyles.colors.primary50,
@@ -43,19 +57,19 @@ const styles = StyleSheet.create({
     description: {
         fontSize: 16,
         marginBottom: 4,
-        fontWeight: 'bold',
+        fontWeight: "bold",
     },
     amountContainer: {
         paddingHorizontal: 12,
         paddingVertical: 4,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
         borderRadius: 4,
         minWidth: 80,
     },
     amount: {
         color: GlobalStyles.colors.primary500,
-        fontWeight: 'bold'
-    }
-})
+        fontWeight: "bold",
+    },
+});
