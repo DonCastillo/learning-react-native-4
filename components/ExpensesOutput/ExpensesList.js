8f635f3395c87a1b85187c55c7ec4edb4979264a
Don <don.qcastillo@yahoo.com>
2023-07-10 00:27:43 -0600

    stylized item cards

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,20 @@
 import { View, Text, FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
+function renderExpenseItem(itemData) {
+    return <ExpenseItem {...itemData.item} />
+}
 
-function ExpensesList() {
-    return <FlatList />
+function ExpensesList({ expenses }) {
+    return (
+        <FlatList
+            data={expenses}
+            renderItem={renderExpenseItem}
+            keyExtractor={(item) => {
+                return item.id
+            }}
+        />
+    );
 }
 
 export default ExpensesList;
