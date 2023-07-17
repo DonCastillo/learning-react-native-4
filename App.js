1a07098dea542e2974d7d56b894bf951dd1ad3ea
Don <don.qcastillo@yahoo.com>
2023-07-16 19:18:15 -0600

    added recent and all expenses page

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import AllExpenses from "./screens/AllExpenses";
 import { GlobalStyles } from "./constants/styles";
 import { Ionicons } from "@expo/vector-icons";
 import IconButton from "./components/UI/IconButton";
+import ExpensesContextProvider from "./store/expenses-context";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -15,7 +16,7 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
     return (
         <BottomTabs.Navigator
-            screenOptions={{
+            screenOptions={({ navigation }) => ({
                 headerStyle: {
                     backgroundColor: GlobalStyles.colors.primary500,
                 },
@@ -24,10 +25,17 @@ function ExpensesOverview() {
                     backgroundColor: GlobalStyles.colors.primary500,
                 },
                 tabBarActiveTintColor: GlobalStyles.colors.accent500,
-                headerRight: ({tintColor}) => (
-                    <IconButton icon="add" size={24} color={tintColor} onPress={() => console.log('Hello')} />
+                headerRight: ({ tintColor }) => (
+                    <IconButton
+                        icon="add"
+                        size={24}
+                        color={tintColor}
+                        onPress={() => {
+                            navigation.navigate("ManageExpense");
+                        }}
+                    />
                 ),
-            }}
+            })}
         >
             <BottomTabs.Screen
                 name="RecentExpenses"
@@ -59,20 +67,32 @@ export default function App() {
     return (
         <>
             <StatusBar style="auto" />
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="ExpensesOverview"
-                        component={ExpensesOverview}
-                        options={{ headerShown: false }}
-                    />
+            <ExpensesContextProvider>
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerStyle: {
+                                backgroundColor: GlobalStyles.colors.primary500,
+                            },
+                            headerTintColor: "white",
+                        }}
+                    >
+                        <Stack.Screen
+                            name="ExpensesOverview"
+                            component={ExpensesOverview}
+                            options={{ headerShown: false }}
+                        />
 
-                    <Stack.Screen
-                        name="ManageExpense"
-                        component={ManageExpenses}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
+                        <Stack.Screen
+                            name="ManageExpense"
+                            component={ManageExpenses}
+                            options={{
+                                presentation: "modal",
+                            }}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </ExpensesContextProvider>
         </>
     );
 }
--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,7 +1,75 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
-function ManageExpenses() {
-    return <Text>Manage Expenses Screen</Text>;
+function ManageExpenses({ route, navigation }) {
+    console.log(route);
+    const editedExpenseId = route.params?.expenseId;
+    const isEditing = !!editedExpenseId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? "Edit Expense" : "Add Expense",
+        });
+    }, [navigation, isEditing]);
+
+    function deleteExpenseHandler() {
+        navigation.goBack();
+    }
+
+    function cancelHandler() {
+        navigation.goBack();
+    }
+
+    function confirmHandler() {
+        navigation.goBack();
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button mode="flat" onPress={cancelHandler} style={styles.button}>Cancel</Button>
+                <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+        </View>
+    );
 }
 
 export default ManageExpenses;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'ce'
+    },
+    button: {
+        width: 120,
+        marginHorizontal: 8,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center' 
+    }
+
+})
