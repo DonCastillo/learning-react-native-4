f99878defad1be9101348ec6a7018322ff9baa60
Don <don.qcastillo@yahoo.com>
2023-07-20 03:47:41 -0600

    able to load db data to the context api

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,16 +1,30 @@
 import { Text } from "react-native";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
     const expensesCtx = useContext(ExpensesContext);
+    // const [fetchedExpenses, setFetchedExpenses] = useState([]);
+
+    useEffect(() => {
+        async function getExpenses() {
+            const expenses = await fetchExpenses();
+            expensesCtx.setExpense(expenses);
+        }
+
+        getExpenses()
+    }, [])
+
     const recentExpenses = expensesCtx.expenses.filter((expense) => {
         const today = new Date();
         const date7DaysAgo = getDateMinusDays(today, 7);
         return (expense.date >= date7DaysAgo) && (expense.date <= today);
     });
+
+
     return (
         <ExpensesOutput
             expenses={recentExpenses}
